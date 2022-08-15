37ceb015e20923e4c49798953e58611ab745e896
guilhermedom <guilhermedfs@gmail.com>
2022-08-14 22:34:42 -0300

    Update app file to support significance level input

--- app/app.R
+++ app/app.R
@@ -13,10 +13,12 @@ ui = fluidPage(
 
     fluidRow(
         column(6, align = "center", fileInput("fileInputID", "Choose a file having the data to test:",
-                            accept = c("text/csv", "text/comma-separated-values", "text/plain", ".csv")),
+                                              accept = c("text/csv", "text/comma-separated-values", "text/plain", ".csv")),
                helpText("Only the first column of the selected file will be evaluated."),
                actionButton("checkButtonID", "Check!")),
-        column(6, align = "center", h2(verbatimTextOutput("pvalueTextID")))
+        column(6, align = "center", textInput("significanceInputID", "Desired significance level for the data normality test
+                                              (default is 0.05):", value = 0.05),
+               h2(verbatimTextOutput("pvalueTextID")))
     ),
     fluidRow(
         column(6, plotOutput("histogramID")),
@@ -24,7 +26,7 @@ ui = fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server = function(input, output) {
 
     observeEvent(input$checkButtonID, {
@@ -32,9 +34,17 @@ server = function(input, output) {
         
         data = read.csv(input$fileInputID$datapath, header = T)[, 1]
         
-        pvalue = shapiro.test(data)[2]
-        pvalue = paste("Shapiro-Wilk test's p-value:", pvalue, "\nDistribution is not normal if p-value <= 0.05.")
-        output$pvalueTextID = renderText({pvalue})
+        pvalue = as.numeric(shapiro.test(data)[2])
+        alpha = input$significanceInputID
+        
+        # rounded value is used to avoid floating point issues
+        pvalueRounded = round(pvalue, 10)
+        if (pvalueRounded < alpha) {
+            pvalueResult = paste("Shapiro-Wilk test's p-value:", pvalue, "\nData distribution is not normal as p-value <", alpha)
+        } else {
+            pvalueResult = paste("Shapiro-Wilk test's p-value:", pvalue, "\nData distribution is normal as p-value >=", alpha)
+        }
+        output$pvalueTextID = renderText({pvalueResult})
         
         output$histogramID = renderPlot({hist(data, main = "Data Histogram")})
         output$qqplotID = renderPlot({
