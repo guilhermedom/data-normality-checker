e13c29878b18ea2c2d6f081b448b2bd5d45188f1
guilhermedom <guilhermedfs@gmail.com>
2022-08-26 21:06:59 -0300

    style: reformat app script for better readability

--- app/app.R
+++ app/app.R
@@ -13,12 +13,20 @@ ui = fluidPage(
     br(),
 
     fluidRow(
-        column(6, align = "center", fileInput("fileInputID", "Choose a file having the data to test:",
-                                              accept = c("text/csv", "text/comma-separated-values", "text/plain", ".csv")),
-               helpText("Only the first column of the selected file will be evaluated."),
+        column(6, align = "center",
+               fileInput("fileInputID", "Choose a file having the data to test:",
+                         accept = c("text/csv",
+                                    "text/comma-separated-values",
+                                    "text/plain",
+                                    ".csv")),
+               helpText("Only the first column of the selected file will be
+                        evaluated."),
                actionButton("checkButtonID", "Check!")),
-        column(6, align = "center", textInput("significanceInputID", "Desired significance level for the data normality test
-                                              (default is 0.05):", value = 0.05),
+        column(6, align = "center",
+               textInput("significanceInputID", 
+                         "Desired significance level for the data normality test
+                         (default is 0.05):",
+                         value = 0.05),
                h2(verbatimTextOutput("pvalueTextID")))
     ),
     fluidRow(
@@ -31,7 +39,9 @@ ui = fluidPage(
 server = function(input, output) {
 
     observeEvent(input$checkButtonID, {
-        validate(need(input$fileInputID, "A file must be selected."))
+        validate(
+            need(input$fileInputID, "A file must be selected.")
+        )
         
         data = read.csv(input$fileInputID$datapath, header = T)[, 1]
         
@@ -41,13 +51,19 @@ server = function(input, output) {
         # rounded value is used to avoid floating point issues
         pvalueRounded = round(pvalue, 10)
         if (pvalueRounded < alpha) {
-            pvalueResult = paste("Shapiro-Wilk test's p-value:", pvalue, "\nData distribution is not normal as p-value <", alpha)
+            pvalueResult = paste("Shapiro-Wilk test's p-value:", pvalue, "\nData
+                                 distribution is not normal as p-value <", alpha)
         } else {
-            pvalueResult = paste("Shapiro-Wilk test's p-value:", pvalue, "\nData distribution is normal as p-value >=", alpha)
+            pvalueResult = paste("Shapiro-Wilk test's p-value:", pvalue, "\nData
+                                 distribution is normal as p-value >=", alpha)
         }
-        output$pvalueTextID = renderText({pvalueResult})
+        output$pvalueTextID = renderText({
+            pvalueResult
+        })
         
-        output$histogramID = renderPlot({hist(data, main = "Data Histogram")})
+        output$histogramID = renderPlot({
+            hist(data, main = "Data Histogram")
+        })
         output$qqplotID = renderPlot({
             qqnorm(data)
             qqline(data)
